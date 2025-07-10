b142f68d8a38b72c6e9735d2331c396f46cc7ae5
AriTtro Sarker <149282649+arittrosarker@users.noreply.github.com>
2025-07-10 21:10:14 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -73,8 +73,7 @@ finalTextSlide.innerHTML = `
   <p>jonmodiner shuveccha! dekhte dekhte ekti bochor par hoye gelo. tor biyer boyosh hoye gelo.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
   <p>iti,</p>
   <p>tor oprio bondhu</p>
-  <p>arittro</p> <br> <br> <br>
-<br>
+  <p>arittro</p> <br> <br>
 `;
 slidesContainer.appendChild(finalTextSlide);
 
